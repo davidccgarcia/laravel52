92e562dc560378bde70bcaa79143f282988b2533
David García <ccristhiangarcia@gmail.com>
2018-05-16 15:16:17 -0500

    Changes in the RouteServiceProvider - mapping web, api and admin routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,8 +37,35 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map(Router $router)
     {
-        $router->group(['namespace' => $this->namespace], function ($router) {
-            require app_path('Http/routes.php');
+        $this->mapWebRoutes($router);
+
+        $this->mapApiRoutes($router);
+    }
+
+    protected function mapWebRoutes(Router $router)
+    {
+        $router->group([
+            'middleware' => 'web', 'namespace' => $this->namespace
+        ], function ($router) {
+            require app_path('Http/routes/web.routes.php');
+        });
+    }
+
+    protected function mapApiRoutes(Router $router)
+    {
+        $router->group([
+            'middleware' => 'api', 'namespace' => $this->namespace . '\Api'
+        ], function ($router) {
+            require app_path('Http/routes/api.routes.php');
+        });
+    }
+
+    protected function mapAdminRoutes(Router $router)
+    {
+        $router->group([
+            'middleware' => 'admi', 'namespace' => $this->namespace . '\Admin'
+        ], function ($router) {
+            require app_path('Htpp/routes/admin.routes.php');
         });
     }
 }
