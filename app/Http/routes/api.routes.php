9af25d4d3e1fd22a151a79707e564fd8058001f6
David García <ccristhiangarcia@gmail.com>
2018-05-28 14:34:49 -0500

    Token-based authentication system

--- app/Http/routes/api.routes.php
+++ app/Http/routes/api.routes.php
@@ -1,8 +1,17 @@
 <?php
 
-Route::get('v1/upper/{word}', function ($word) {
-    return [
-        'original' => $word, 
-        'upper' => strtoupper($word)
-    ];
+Route::group(['prefix' => 'v1'], function () {
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('upper/{word}', function ($word) {
+            return [
+                'original' => $word, 
+                'upper' => strtoupper($word)
+            ];
+        });
+
+        Route::get('profile', function () {
+            return auth()->guard('api')->user();
+        });
+    });
 });
