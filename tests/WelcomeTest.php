61fd7dd080f50aa230cd850f7b2e180d74f39b2a
David García <ccristhiangarcia@gmail.com>
2018-06-20 16:13:47 -0500

    Test with the seeText method

--- tests/WelcomeTest.php
+++ tests/WelcomeTest.php
@@ -6,6 +6,6 @@ class WelcomeTest extends TestCase
     function test_welcome_user()
     {
         $this->visit('/')
-            ->see('Bienvenido a <b>Styde</b>');
+            ->seeText('Bienvenido a Styde');
     }
 }
